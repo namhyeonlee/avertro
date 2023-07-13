c397b2227daddca2567685750d90efb8ee17d785
namhyeon <namhyeon5000@gmail.com>
2023-07-14 00:27:45 +1000

    Develop [Strategic Business Objectives]  tab component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,109 +1,210 @@
 import { useState } from "react";
+import TabList from "components/common/TabList";
+import DatePickerInput from "components/common/DatePickerInput";
 import { Button } from "@mui/material";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
+import { doGetByteLength, toastCall } from "utils/commonFunction";
+
+interface IObjective {
+  id: number;
+  objective: string;
+  keyMeasures: IKeyMeasure[];
+  startDate: string;
+  endDate: string;
+}
+
+interface IKeyMeasure {
+  id: number;
+  value: string;
+}
+
+const initKeyMeasure: IKeyMeasure = {
+  id: 1,
+  value: "",
+};
+
+const initObjective: IObjective = {
+  id: 1,
+  objective: "",
+  keyMeasures: [initKeyMeasure],
+  startDate: "",
+  endDate: "",
+};
+
+const MAX_OBJECTIVE = 3;
+const ERROR_MSG = "Invalid Date";
 
 const MainPage = () => {
-  const [value, setValue] = useState(null);
+  const [lstObjective, setLstObjective] = useState<IObjective[]>([initObjective]);
+
+  const doAddObjective = () => {
+    if (lstObjective.length === MAX_OBJECTIVE) {
+      toastCall("You can add up to three maximum.", "warning");
+    } else {
+      setLstObjective([...lstObjective, { ...initObjective, id: lstObjective.length + 1 }]);
+    }
+  };
+
+  const doAddKeyMeasures = (id: number, length: number) => {
+    if (length === MAX_OBJECTIVE) {
+      toastCall("You can add up to three maximum.", "warning");
+    } else {
+      setLstObjective(
+        lstObjective.map((obj) =>
+          obj.id === id
+            ? {
+                ...obj,
+                keyMeasures: [...obj.keyMeasures, { ...initKeyMeasure, id: length + 1 }],
+              }
+            : obj
+        )
+      );
+    }
+  };
+
+  const onChangeValue = (id: number, key: string, value: string) => {
+    setLstObjective(lstObjective.map((obj) => (obj.id === id ? { ...obj, [key]: value } : obj)));
+  };
+
+  const doUpdateObjective = (obj: IObjective) => {
+    if (!doVaildation(obj)) return;
+
+    localStorage.setItem(`obj${obj.id}`, JSON.stringify(obj));
+  };
+
+  const doVaildation = (obj: IObjective) => {
+    if (doGetByteLength(obj.objective) > 20) {
+      toastCall("You cannot exceed 20 characters.", "error");
+      return false;
+    }
+    for (let i = 0; i < obj.keyMeasures.length; i++) {
+      if (doGetByteLength(obj.keyMeasures[i].value) > 20) {
+        toastCall("You cannot exceed 20 characters.", "error");
+        return false;
+      }
+    }
+    if (!obj.startDate || obj.startDate === ERROR_MSG) {
+      toastCall("Please enter the starting date.", "error");
+      return false;
+    }
+    if (!obj.endDate || obj.startDate === ERROR_MSG) {
+      toastCall("Please enter the end date.", "error");
+      return false;
+    }
+    if (Number(obj.startDate) > Number(obj.endDate)) {
+      toastCall("Please verify the start date and end date.", "error");
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div className="container">
       <div className="titleWrap">
         <p className="title">Set Security Strategy</p>
       </div>
       <div className="contents">
-        <div className="lstTab">
-          <div>Mission & Vision</div>
-          <div>Strategic Business Objectives</div>
-        </div>
-        <div className="contentWrap">
-          <div className="card">
-            <div className="cardWrap">
-              <div className="cardLeft">
-                <div className="cardContent">
-                  <div className="title">Objective1</div>
-                  <input type="text" />
-                </div>
-                <div className="cardContent">
-                  <div className="titleBox">
-                    <p className="title">Key Measures</p>
-                    <div className="subTitleWrap">
-                      <p className="subTitle">Add additional key measure</p>
-                      <AddCircleIcon
-                        style={{
-                          color: "#25397d",
-                          fontSize: "18px",
-                          cursor: "pointer",
-                          marginLeft: "5px",
-                        }}
-                      />
+        <TabList titles={["Mission & Vision", "Strategic Business Objectives"]}>
+          <div className="contentWrap">Mission & Vision</div>
+          <div className="contentWrap">
+            {lstObjective.map((rowData, i) => (
+              <div key={`card${i}`} className="card">
+                <div className="cardWrap">
+                  <div className="cardLeft">
+                    <div className="cardContent">
+                      <div className="title">Objective{i + 1}</div>
+                      <input type="text" value={rowData.objective} onChange={(e) => onChangeValue(rowData.id, "objective", e.target.value)} />
+                    </div>
+                    <div className="cardContent">
+                      <div className="titleBox">
+                        <p className="title">Key Measures</p>
+                        <div className="subTitleWrap">
+                          <p className="subTitle">Add additional key measure</p>
+
+                          <a onClick={() => doAddKeyMeasures(rowData.id, rowData.keyMeasures.length)}>
+                            <AddCircleIcon style={{ color: "#25397d", fontSize: "18px", cursor: "pointer", marginLeft: "5px" }} />
+                          </a>
+                        </div>
+                      </div>
+                      {rowData.keyMeasures.map((keyMeasure, i) => (
+                        <div key={`input${i}`} className="inputWrap">
+                          <input
+                            type="text"
+                            value={keyMeasure.value}
+                            onChange={(e) =>
+                              setLstObjective(
+                                lstObjective.map((obj) =>
+                                  obj.id === rowData.id
+                                    ? {
+                                        ...obj,
+                                        keyMeasures: obj.keyMeasures.map((key) => (key.id === keyMeasure.id ? { ...key, value: e.target.value } : key)),
+                                      }
+                                    : obj
+                                )
+                              )
+                            }
+                          />
+                          {rowData.keyMeasures.length - 1 !== i && (
+                            <a
+                              className="removeIcon"
+                              onClick={() =>
+                                setLstObjective(
+                                  lstObjective.map((obj) =>
+                                    obj.id === rowData.id
+                                      ? {
+                                          ...obj,
+                                          keyMeasures: obj.keyMeasures.filter((key) => key.id !== keyMeasure.id).map((data, i) => ({ ...data, id: i + 1 })),
+                                        }
+                                      : obj
+                                  )
+                                )
+                              }
+                            >
+                              <RemoveCircleIcon style={{ color: "#e03345", fontSize: "18px", cursor: "pointer", marginLeft: "10px" }} />
+                            </a>
+                          )}
+                        </div>
+                      ))}
                     </div>
                   </div>
-                  <div className="inputWrap">
-                    <input type="text" />
-                    <RemoveCircleIcon
-                      style={{
-                        color: "#e03345",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        marginLeft: "10px",
-                      }}
-                    />
+                  <div className="cardRight">
+                    <div className="startDate">
+                      <div className="title">Start Date</div>
+                      <DatePickerInput date={rowData.startDate} maxDate={rowData.endDate} onChange={(date) => onChangeValue(rowData.id, "startDate", date)} />
+                    </div>
+                    <div className="endDate">
+                      <div className="title">End Date</div>
+                      <DatePickerInput date={rowData.endDate} minDate={rowData.startDate} onChange={(date) => onChangeValue(rowData.id, "endDate", date)} />
+                    </div>
                   </div>
+                </div>
+                <div className="btnWrap">
+                  <Button
+                    className="delBtn"
+                    variant="outlined"
+                    onClick={() => {
+                      setLstObjective(lstObjective.filter((obj) => obj.id !== rowData.id).map((data, i) => ({ ...data, id: i + 1 })));
+                      localStorage.removeItem(`obj${rowData.id}`);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                  <Button className="updateBtn" onClick={() => doUpdateObjective(rowData)}>
+                    Update
+                  </Button>
                 </div>
               </div>
-              <div className="cardRight">
-                <div className="startDate">
-                  <div className="title">Start Date</div>
-                  <div className="datePicker">
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                      <DatePicker
-                        slotProps={{ textField: { size: "small" } }}
-                        value={value}
-                        onChange={(newValue) => setValue(newValue)}
-                      />
-                    </LocalizationProvider>
-                  </div>
-                </div>
-                <div className="endDate">
-                  <div className="title">End Date</div>
-                  <div className="datePicker">
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                      <DatePicker
-                        slotProps={{
-                          textField: { size: "small" },
-                        }}
-                        value={value}
-                        onChange={(newValue) => setValue(newValue)}
-                      />
-                    </LocalizationProvider>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="btnWrap">
-              <Button className="delBtn" variant="outlined">
-                Delete
+            ))}
+
+            <div className="addBtnWrap">
+              <Button className="addBtn" onClick={() => doAddObjective()}>
+                <AddCircleIcon style={{ color: "#fff", fontSize: "18px", cursor: "pointer", marginRight: "5px" }} />
+                Add Objective
               </Button>
-              <Button className="updateBtn">Update</Button>
             </div>
           </div>
-          <div className="addBtnWrap">
-            <Button className="addBtn">
-              <AddCircleIcon
-                style={{
-                  color: "#fff",
-                  fontSize: "18px",
-                  cursor: "pointer",
-                  marginRight: "5px",
-                }}
-              />
-              Add Objective
-            </Button>
-          </div>
-        </div>
+        </TabList>
       </div>
     </div>
   );
