aa9f82a9cd06bd4cc320f5ef19609a157b08697a
namhyeon <namhyeon5000@gmail.com>
2023-07-14 00:45:55 +1000

    build

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
